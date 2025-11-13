7161eac45f812755c58979446012bc106acdedfb
Erick Vicentin <erickvicentin14@hotmail.com>
2025-11-12 23:48:57 -0300

    feat: update de Main con prueba de creacion de Motocicleta

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import enums.TipoDeMotocicleta;
+import vehiculos.Motocicleta;
+import vehiculos.Vehiculo;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        vehiculos.add(
+                new Motocicleta(
+                        "Honda",
+                        "Wave 110s",
+                        2018,
+                        "Negra",
+                        21500,
+                        TipoDeMotocicleta.CROSS
+                )
+        );
+
+        for (Vehiculo vehiculo : vehiculos) {
+            System.out.println(vehiculo);
+            System.out.println("Es usado el vehiculo: " + (vehiculo.isUsado() ? "si" : "no"));
         }
     }
 }
